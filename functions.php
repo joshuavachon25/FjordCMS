57ee4286d78b404118fd534dab5bf3822b5741a0
Joshua Vachon <46571227+joshuavachon25@users.noreply.github.com>
2023-07-30 00:59:38 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,17 +1,3 @@
 <?php
     //include 'customs/NameOfYourCustomPostType.php';
-
-    add_filter( 'allowed_block_types', 'cyberfjord_allowed_block_types' );
- 
-    function cyberfjord_allowed_block_types( $allowed_blocks ) {
-      
-      return array(
-        'core/image',
-        'core/paragraph',
-        'core/heading',
-        'core/quote',
-        'core/list'
-      );
-     
-    }
 ?>
